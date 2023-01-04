13afd542fc57ca8f7107c2aa5d387bf9cedbe5dd
LoickDev97438 <loickcherimont@gmail.com>
2023-01-04 22:42:22 +0100

    Verify request is set and valid before call API

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import { fetchFranceCities } from "./utils/functions.js";
 
-// Back Work
+// GLOBAL
+const emptyError = document.querySelector('#error');
 const cities = await fetchFranceCities("https://geo.api.gouv.fr/communes");
 
 // Process user request if form is submitted
@@ -11,12 +12,22 @@ document.getElementById('form')
 function getCityInformations(e) {
     e.preventDefault();
 
-    const userRequestCity = document.getElementById('userRequestCity').value;
+    const userRequestCity = document.getElementById('userRequestCity').value.trim();
+
+    checkIfEmpty(userRequestCity);
+
+    checkIfValid(userRequestCity);
+
+    // console.log(onlyNames);
+    // return;
 
     // Display requested city and its informations
     cities.forEach(city => {
-        if(city['nom'] === userRequestCity) {
-            document.getElementById('cityName').innerText = userRequestCity;
+        // User's request becomes case-insensitive
+        let caseInsensitiveCity = city['nom'].toLowerCase();
+
+        if(caseInsensitiveCity === userRequestCity.toLowerCase()) {
+            document.getElementById('cityName').innerText = city['nom'];
             document.getElementById('cityDepartmentCode').innerText = city['codeDepartement'];
             document.getElementById('cityPopulation').innerText = city['population'];
             document.getElementById('cityZips').innerText = city['codesPostaux'];
@@ -26,4 +37,32 @@ function getCityInformations(e) {
 
     this.reset();
 
+}
+
+// If empty returns error message
+function checkIfEmpty(userRequestCity) {
+    if(!userRequestCity) {
+        emptyError.classList.remove('d-none');
+        return;
+    } else {
+        emptyError.classList.add('d-none');
+    }
+    
+}
+
+function checkIfValid(userRequestCity) {
+    let onlyNames = [];
+
+    for(const city of cities) {
+        onlyNames.push(city['nom']);
+    }
+
+    onlyNames = onlyNames.map(city => city.toLowerCase());
+
+    if(onlyNames.indexOf(userRequestCity.toLowerCase()) === -1) {
+        emptyError.classList.remove('d-none');
+        return;
+    }
+
+    emptyError.classList.add('d-none');
 }
