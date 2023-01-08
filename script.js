31050f29d2988ddcc0dd36b2e77211b5c6a5e523
LoickDev97438 <loickcherimont@gmail.com>
2023-01-08 16:30:13 +0100

    Structure code and process error from fetch

--- script.js
+++ script.js
@@ -1,75 +1,74 @@
 import { clearPreviousResults, countResults, fetchFranceCities } from "./utils/functions.js";
 
-// GLOBAL
-const emptyError = document.querySelector('#error');
-const cities = await fetchFranceCities("https://geo.api.gouv.fr/communes");
-
-// Process user request if form is submitted
-document.getElementById('form')
-        .addEventListener('submit', getCityInformations);
-
-
-function getCityInformations(e) {
-    e.preventDefault();
-
-    clearPreviousResults();
-
-    // PROCESS
-    // Clean user request
-    const userRequestCity = document.getElementById('userRequestCity').value.trim();
-
-    checkIfEmpty(userRequestCity);
-
-    checkIfValid(userRequestCity);
-
-    // Display requested city and its informations
-    cities.forEach(city => {
-        // User's request becomes case-insensitive
-        let caseInsensitiveCity = city['nom'].toLowerCase();
-
-        if(caseInsensitiveCity === userRequestCity.toLowerCase()) {
-            let cityCardTemplate = document.getElementById('cardLayout').content.cloneNode(true);
-            let cityCard = cityCardTemplate.querySelector('.card');
-
-            cityCard.querySelector('#cityName').innerText = city['nom'];
-            cityCard.querySelector('#cityDepartmentCode').innerText = city['codeDepartement'];
-            cityCard.querySelector('#cityPopulation').innerText = city['population'];
-            cityCard.querySelector('#cityZips').innerText = city['codesPostaux'];
-
-            document.querySelector('#results').append(cityCard);
-
-            // Debug: 
-            console.log(city);
-        }
-    })
-
-    countResults();
-
-    this.reset();
-
+try {
+    const cities = await fetchFranceCities("https://geo.api.gouv.fr/communes");
+    process(cities);
+} catch(e) {
+    console.error(e);
 }
 
-// If empty returns error message
-function checkIfEmpty(userRequestCity) {
-    if(!userRequestCity) {
+function process(cities) {
+    // Process user request if form is submitted
+    document.getElementById('form')
+            .addEventListener('submit', (e) => {
+                    e.preventDefault();
+                
+                    clearPreviousResults();
+
+                
+                    // PROCESS
+                    // Clean user request
+                    const userRequestCity = document.getElementById('userRequestCity').value.trim();
+
+                    prepareUserRequest(userRequestCity, cities);
+                
+                    // Display requested city and its informations
+                    cities.forEach(city => {
+                        // User's request becomes case-insensitive
+                        let caseInsensitiveCity = city['nom'].toLowerCase();
+                
+                        if (caseInsensitiveCity === userRequestCity.toLowerCase()) {
+                            let cityCardTemplate = document.getElementById('cardLayout').content.cloneNode(true);
+                            let cityCard = cityCardTemplate.querySelector('.card');
+                
+                            cityCard.querySelector('#cityName').innerText = city['nom'];
+                            cityCard.querySelector('#cityDepartmentCode').innerText = city['codeDepartement'];
+                            cityCard.querySelector('#cityPopulation').innerText = city['population'];
+                            cityCard.querySelector('#cityZips').innerText = city['codesPostaux'];
+                
+                            document.querySelector('#results').append(cityCard);
+                        }
+                    });
+                
+                    countResults();
+                
+                    e.currentTarget.reset();
+            });
+}
+
+function prepareUserRequest(userRequestCity, cities) {
+
+    let onlyNames = [];
+    const emptyError = document.querySelector('#error');
+
+    // Check if request is empty
+    // to display error message
+    if (!userRequestCity) {
         emptyError.classList.remove('d-none');
         return;
     } else {
         emptyError.classList.add('d-none');
     }
-    
-}
 
-function checkIfValid(userRequestCity) {
-    let onlyNames = [];
-
-    for(const city of cities) {
+    // Check if request is valid
+    // if city exists
+    for (const city of cities) {
         onlyNames.push(city['nom']);
     }
 
     onlyNames = onlyNames.map(city => city.toLowerCase());
 
-    if(onlyNames.indexOf(userRequestCity.toLowerCase()) === -1) {
+    if (onlyNames.indexOf(userRequestCity.toLowerCase()) === -1) {
         emptyError.classList.remove('d-none');
         return;
     }
