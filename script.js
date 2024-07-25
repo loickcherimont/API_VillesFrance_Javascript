21a6966e8ab37df4aab40158a2f8a224ee749fa9
loickcherimont <loickcherimont@gmail.com>
2024-07-25 20:05:16 +0200

    Reduce code and separate process() into differents functions

--- script.js
+++ script.js
@@ -1,112 +1,87 @@
-import { clearPreviousResults, countResults, fetchFranceCities } from "./utils/functions.js";
+import { countResults, fetchFranceCities, uiReset } from "./utils/functions.js";
+
+const cities = await fetchFranceCities("https://geo.api.gouv.fr/communes");
+
 
 try {
-    const cities = await fetchFranceCities("https://geo.api.gouv.fr/communes");
-    process(cities);
-} catch(e) {
+    // const cities = await fetchFranceCities("https://geo.api.gouv.fr/communes");
+    // process(cities);
+} catch (e) {
     console.error(e);
 }
 
-function process(cities) {
-    // Process user request if form is submitted
-    document.getElementById('form')
-            .addEventListener('submit', (e) => {
-                    e.preventDefault();
-                
-                    clearPreviousResults();
+document.getElementById('form')
+    .addEventListener('submit', (e) => {
+        e.preventDefault();
+        uiReset();
+        const form = e.target;
 
-                
-                    // PROCESS
-                    // Clean user request
-                    let userRequestCity = document.getElementById('userRequestCity').value.trim();
+        let formData = new FormData(form);
 
-                    userRequestCity = controlEntry(userRequestCity, cities);
+        let q = prepare(formData.get('user_request'), cities);
+        uiDisplay(q, cities);
+        countResults();
+        form.reset();
+    })
 
-                    if(!userRequestCity) {
-                        e.currentTarget.reset();
-                        return;
-                    }
-                
-                    // Display requested city and its informations
-                    cities.forEach(city => {
-                        // User's request becomes case-insensitive
-                        let caseInsensitiveCity = city['nom'].toLowerCase();
-                
-                        if (caseInsensitiveCity === userRequestCity.toLowerCase()) {
-                            let cityCardTemplate = document.getElementById('cardLayout').content.cloneNode(true);
-                            let cityCard = cityCardTemplate.querySelector('.card');
-                
-                            cityCard.querySelector('#cityName').innerText = city['nom'];
-                            cityCard.querySelector('#cityDepartmentCode').innerText = city['codeDepartement'];
-                            cityCard.querySelector('#cityPopulation').innerText = city['population'];
-                            cityCard.querySelector('#cityZips').innerText = city['codesPostaux'];
-                
-                            document.querySelector('#results').append(cityCard);
-                        }
-                    });
-                
-                    countResults();
-                
-                    e.currentTarget.reset();
-            });
-}
-
-function controlEntry(userRequestCity, cities) {
-
+// Clean the query
+// Check if it is valid
+// Before the API works
+// Parameters: query string and data json
+function prepare(query, data) {
     let onlyNames = [];
     const emptyError = document.querySelector('#error');
 
+
+    query = query.trim();
+
+    console.log(query, query.length)
+
     // Check if request is empty
     // to display error message
-    if (!userRequestCity) {
+    if (!query) {
         emptyError.classList.remove('d-none');
-        return userRequestCity
+        return query
     } else {
         emptyError.classList.add('d-none');
     }
 
-    // Check if request is valid
-    // if city exists
-    for (const city of cities) {
-        onlyNames.push(city['nom']);
-    }
+    // Create an array containing only city names
+    onlyNames = data.map(city => city['nom'].toLowerCase());
 
-    // onlyNames = onlyNames.map(city => city.toLowerCase());
-    onlyNames = onlyNames.map(city => cancelSpecialLetters(city));
-
-    // *** Debug
-    console.log(onlyNames);
-    console.log(userRequestCity);
-    console.log(onlyNames.includes(userRequestCity.toLowerCase()));
-    // End Debug ***
-
-    if (onlyNames.indexOf(userRequestCity.toLowerCase()) === -1) {
+    if (onlyNames.indexOf(query.toLowerCase()) === -1) {
+        console.log("N'existe pas!")
         emptyError.classList.remove('d-none');
         return null
+    } else {
+        console.log("Existe!")
+        emptyError.classList.add('d-none');
     }
 
-    emptyError.classList.add('d-none');
-    return userRequestCity;
+    return query;
 }
 
-function cancelSpecialLetters(cityName) {
-    cityName = cityName.toLowerCase();
-    cityName = cityName.replaceAll('à', 'a')
-                       .replaceAll('â', 'a')
-                       .replaceAll('æ', 'a');
-    cityName = cityName.replaceAll('ç', 'c');
-    cityName = cityName.replaceAll('é', 'e')
-                       .replaceAll('è', 'e')
-                       .replaceAll('ê', 'e')
-                       .replaceAll('ë', 'e');
-    cityName = cityName.replaceAll('î', 'i')
-                       .replaceAll('ï', 'i');
-    cityName = cityName.replaceAll('ô', 'o')
-                       .replaceAll('œ', 'o');
-    cityName = cityName.replaceAll('ù', 'u')
-                       .replaceAll('û', 'u')
-                       .replaceAll('ü', 'u');
-    cityName = cityName.replaceAll('ÿ', 'y');
+// Display on the screen the results
+// Of user query
+// Parameters: query string and data json
+function uiDisplay(query, data) {
 
-    return cityName;
+    data.forEach(city => {
+        
+        // To ease the research
+        // Change normal case into lower case
+        let caseInsensitiveCity = city['nom'].toLowerCase();
+
+        if (caseInsensitiveCity === query.toLowerCase()) {
+            let cityCardTemplate = document.getElementById('cardLayout').content.cloneNode(true);
+            let cityCard = cityCardTemplate.querySelector('.card');
+
+            cityCard.querySelector('#cityName').innerText = city['nom'];
+            cityCard.querySelector('#cityDepartmentCode').innerText = city['codeDepartement'];
+            cityCard.querySelector('#cityPopulation').innerText = city['population'];
+            cityCard.querySelector('#cityZips').innerText = city['codesPostaux'];
+
+            document.querySelector('#results').append(cityCard);
+        }
+    });
 }
